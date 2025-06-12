f4662d327eda234084e3c7c531c3bb77b0642c6d
BreadBagel <killerwhale019@gmail.com>
2025-06-12 20:25:37 +0800

    Add/update requirements.txt and config.py

--- config.py
+++ config.py
@@ -1,25 +1,24 @@
 #config.py
 
 import os
-
 class Config:
     UPLOAD_FOLDER = 'uploads'
     SPECTROGRAM_FOLDER = 'spectrograms'
     ALLOWED_EXTENSIONS = {'wav', 'mp3', '3gp'}
 
     # Load from environment or defaults
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_NAME = os.getenv('DB_NAME', 'deepbreath')
-    DB_USER = os.getenv('DB_USER', 'root')
-    DB_PASS = os.getenv('DB_PASS', 'Alyas-icebear03')
+    DB_HOST = 'deepbreath-db.cz20cso6qfk8.us-west-2.rds.amazonaws.com'
+    DB_NAME = 'deepbreath'
+    DB_USER = 'admin'
+    DB_PASS = 'DeepBreath2425'
     DB_CHARSET = 'utf8mb4'
-    SECRET_KEY = os.getenv('SECRET_KEY', '')
+    SECRET_KEY = 'your-secure-secret-key'
 
     # Build one SQLALCHEMY_DATABASE_URI string
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
-        f"?charset={DB_CHARSET}"
+        f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset={DB_CHARSET}"
     )
+
     # Turn off FSADeprecationWarning
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -32,7 +31,7 @@ class Config:
     # (Optional) If you want cookies instead for refresh tokens:
     # JWT_COOKIE_SECURE  = False
     # JWT_COOKIE_CSRF_PROTECT = True
-
+    
     @classmethod
     def get_db_config(cls):
         return {
